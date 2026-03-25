fed12281befa04b34bc907549f8a910fd2d0a853
Michael-77cm <michaelbello175@gmail.com>
2026-03-25 15:08:56 +0000

    Stop tracking env.py

--- env.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:npg_TVDWXLNuQF84@ep-dry-dew-ag3sbe00.c-2.eu-central-1.aws.neon.tech/those_lung_blade_600749")
-
-os.environ.setdefault("SECRET_KEY",'VH6cP'django-insecure-9jkm=mzu8j_s48@!ex$&w^_d5p8w-04+n88na5mlvh#+w(qbf!'')
-
-
