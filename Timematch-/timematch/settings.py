5550a370183a6feddb6a4c87342ca9ffd7f39a8d
Michael-77cm <michaelbello175@gmail.com>
2026-03-26 12:29:40 +0000

    files updated

--- Timematch-/timematch/settings.py
+++ Timematch-/timematch/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'timematch.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR.parent / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
